aac2cb7271bfde09f23196b33c5b38cb4262c164
TheBeautyofLife <stephaniekirathe@gmail.com>
2019-11-22 00:25:32 +0300

    adding the web dir

---
+++ web/main.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-new */
+new Vue({
+    el: "#app",
+    data: {
+      msg: "Sorry, This website is Currently Unavaliable",
+      //image size
+      image: "https://ik.imagekit.io/qpt2onjfe/Tlogo_cnQmm7JpF.svg",
+      h: 'auto',
+      w: '400'      
+    }
+
+  });
